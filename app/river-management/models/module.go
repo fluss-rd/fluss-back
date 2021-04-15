04e174cdbbc3e154081b9dbbe26c03b7520a4c24
Denys <drosario@truora.com>
2021-04-15 17:31:36 -0400

    add extra fields AB#145

--- app/river-management/models/module.go
+++ app/river-management/models/module.go
@@ -1,7 +1,13 @@
 package models
 
-// Module
+import "time"
+
+// Module represents a device on the river, the entity that is going to take the measurements
 type Module struct {
-	ModuleID string `json:"moduleID" bson:"_id"`
-	RiverID  string `json:"riverID" bson:"riverID"`
+	ModuleID     string    `json:"moduleID" bson:"_id"`
+	PhoneNumber  string    `json:"phoneNumber" bson:"phoneNumber"`
+	RiverID      string    `json:"riverID" bson:"riverID"`
+	UserID       string    `json:"userID" bson:"userID"`
+	CreationDate time.Time `json:"creationDate" bson:"creationDate"`
+	UpdateDate   time.Time `json:"updateDate" bson:"updateDate"`
 }
