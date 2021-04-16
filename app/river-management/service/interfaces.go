a13346dcb1c8ebf75cae2a2b8a4b861429fb37fc
Denys <drosario@truora.com>
2021-04-15 22:24:43 -0400

    return created resource on create functions

--- app/river-management/service/interfaces.go
+++ app/river-management/service/interfaces.go
@@ -8,13 +8,13 @@ import (
 
 // RiversUsecases defines the use cases functions related to rivers
 type RiversUsecases interface {
-	CreateRiver(ctx context.Context, river models.River) error
+	CreateRiver(ctx context.Context, river models.River) (models.River, error)
 	GetRiversN(ctx context.Context) ([]models.River, error) //  no pagination
 }
 
 // ModulesUsecases defines the use cases functions related to modules
 type ModulesUsecases interface {
-	CreateModule(ctx context.Context, module models.Module) error
+	CreateModule(ctx context.Context, module models.Module) (models.Module, error)
 	GetModule(ctx context.Context, moduleID string) (models.Module, error)
 	GetModulesN(ctx context.Context) ([]models.Module, error)                        // no pagination
 	GetModulesByRiverN(ctx context.Context, riverID string) ([]models.Module, error) // no pagination
