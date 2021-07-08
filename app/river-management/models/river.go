5924e1a6cb6692183443ee7be66ee6e9bf342778
Denys <drosario@truora.com>
2021-07-08 16:04:44 -0400

    add print statement when getting rivers fails

--- app/river-management/models/river.go
+++ app/river-management/models/river.go
@@ -27,15 +27,15 @@ var validBodytTypes = map[BodyType]bool{
 type River struct {
 	RiverID      string    `json:"riverID" bson:"_id"` // This is "coupling" bewteen the models and mongo
 	Name         string    `json:"name" bson:"name"`
-	Location     Location  `json:"location" bson:"location"`
+	Location     []Point   `json:"location" bson:"location"`
 	UserID       string    `json:"userID" bson:"userID"`
 	Type         BodyType  `json:"type" bson:"type"`
 	CreationDate time.Time `json:"creationDate" bson:"creationDate"`
 	UpdateDate   time.Time `json:"updateDate" bson:"updateDate"`
 }
 
-// Location location of the entity
-type Location struct {
+// Point represents a coordinate
+type Point struct {
 	Lat float64 `json:"latitude" bson:"latitude"`
 	Lng float64 `json:"longitude" bson:"longitude"`
 }
